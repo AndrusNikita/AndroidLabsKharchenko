ba6844976f83cc455945ce7ecddcbe68acb24cb1
AndrusNikita <53003369+AndrusNikita@users.noreply.github.com>
2021-04-17 14:14:05 +0300

    Lab3(done)

--- app/src/main/java/com/example/lab2kharchenko/MainActivity.java
+++ app/src/main/java/com/example/lab2kharchenko/MainActivity.java
@@ -2,11 +2,20 @@ package com.example.lab2kharchenko;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import static java.lang.Math.*;
 
@@ -16,24 +25,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView res = (TextView) findViewById(R.id.fileText);
+        res.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void onButtonClick (View v){
-        EditText X = (EditText)findViewById(R.id.xIn);
-        TextView Answer = (TextView) findViewById(R.id.AnswerText);
+        EditText X = (EditText)findViewById(R.id.xenter);
+        EditText step = (EditText)findViewById(R.id.stepenter);
+        EditText curCount = (EditText)findViewById(R.id.countEnt);
+        TextView res = (TextView) findViewById(R.id.fileText);
+
 
         double x = Double.parseDouble(X.getText().toString());
-        int e = 5;
-        int a = 5;
-        int z = 5;
-        double Ans = ((Math.pow(z+(sqrt(z*x)),-5))/((pow(e,x))+((pow(a,5))*(atan(x)))));
+        int count = Integer.parseInt(curCount.getText().toString());
+        double Step = Double.parseDouble(step.getText().toString());
+        int e = 2;
+        int a = 3;
+        int z = 4;
+        double Res;
+        String textRes = "";
 
-        Answer.setText(Double.toString(Ans));
+        try {
+            FileOutputStream fileOutput = openFileOutput("Lab3text.txt", MODE_PRIVATE);
+
+            String stepStr = "Step = "+Step;
+            String countStr = "\nMax count of X = "+count;
+
+            fileOutput.write(stepStr.getBytes());
+            fileOutput.write(countStr.getBytes());
+
+            do {
+                Res = ((Math.pow(z+(sqrt(z*x)),-5))/((pow(e,x))+((pow(a,5))*(atan(x)))));
+                textRes = textRes+"\n________________________________________________________\nX = "+x+";\nResult = "+Res;
+
+
+                x = x + Step;
+            } while (x<=count);
+
+            fileOutput.write(textRes.getBytes());
+            fileOutput.close();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            FileInputStream fileInput = openFileInput("Lab3text.txt");
+            InputStreamReader reader = new InputStreamReader(fileInput);
+            BufferedReader buffer = new BufferedReader(reader);
+            StringBuilder strBuffer =  new StringBuilder();
+            String ResLines;
+            while ((ResLines = buffer.readLine()) != null) {
+                strBuffer.append(ResLines).append("\n");
+            }
+            res.setText(strBuffer.toString());
+            fileInput.close();
+        } catch (FileNotFoundException r) {
+            r.printStackTrace();
+        } catch (IOException r) {
+            r.printStackTrace();
+        }
     }
-
-    public void onInfoClick (View v){
-        Intent infoGo = new Intent(".Info");
-        startActivity(infoGo);
-    }
-
 }
